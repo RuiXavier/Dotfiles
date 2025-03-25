499772179c3f3c4b1cc1e7fcdc2712d0481ad1b9
Rui Xavier <ruiacondutoxavier@gmail.com>
2025-03-25 11:56:34 +0000

    Add various configuration files and scripts for setup

---
+++ i3/scripts/greenclip.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+killall greenclip
+greenclip clear 
+greenclip daemon &
---
+++ i3/scripts/redshift.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Run redshift
+
+#sleep 5 seconds
+sleep 5
+
+redshift -c ~/.config/redshift/redshift.conf
